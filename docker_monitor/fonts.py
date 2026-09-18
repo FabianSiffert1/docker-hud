9ed2898107cc4a09ad5baae3b8f51a07988ac25a
FabianSiffert1 <siffert@sipgate.de>
2026-09-18 10:14:34 +0200

    ⚡️ cache fonts and find them on macOS too

--- docker_monitor/fonts.py
+++ docker_monitor/fonts.py
@@ -1,13 +1,18 @@
 """Font loading helpers."""
 
+from functools import lru_cache
+
 from PIL import ImageFont
 
 _FONT_CANDIDATES = [
     "/usr/share/fonts/truetype/dejavu/DejaVuSansMono-Bold.ttf",
     "/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf",
+    "/System/Library/Fonts/Menlo.ttc",
+    "/System/Library/Fonts/Monaco.ttf",
 ]
 
 
+@lru_cache(maxsize=None)
 def get_font(size=14):
     for path in _FONT_CANDIDATES:
         try:
